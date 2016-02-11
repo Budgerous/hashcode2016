87524b66f462968e483cf5bd1fb63dadd05f750a
António Lima <tocalagarto93@gmail.com>
2016-02-11 19:19:14 +0000

    added main.py file

--- input.py
+++ input.py
@@ -1,5 +1,6 @@
-
 '''
+example input file:
+-------------------
 100 100 3 50 500
 3
 100 5 450
@@ -21,60 +22,56 @@
 '''
 
 
-file_path = "input.txt"
+def parse_input_file(file_path):
+    f = open(file_path, 'r')
 
-f = open(file_path, 'r')
+    lines = f.readlines()
 
-lines = f.readlines()
+    # global limits
 
-# global limits
+    num_rows, num_columns, num_drones, num_turns, max_payload = [int(x) for x in lines[0].strip().split()]
+    # print(num_rows, num_columns, num_drones, num_turns, max_payload, "# num_rows, num_columns, num_drones, num_turns, max_payload")
 
-num_rows, num_columns, num_drones, num_turns, max_payload = [int(x) for x in lines[0].strip().split()]
-print(num_rows, num_columns, num_drones, num_turns, max_payload, "# num_rows, num_columns, num_drones, num_turns, max_payload")
+    # products
 
-# products
+    num_product_types = int(lines[1])
+    # print(num_product_types, "# num_product_types")
 
-num_product_types = int(lines[1])
-print(num_product_types, "# num_product_types")
+    product_types_weights = [int(x) for x in lines[2].strip().split()]
+    # print(product_types_weights, "# product_types_weights")
 
-product_types_weights = [int(x) for x in lines[2].strip().split()]
-print(product_types_weights, "# product_types_weights")
+    # warehouses
 
-# warehouses
+    num_warehouses = int(lines[3])
+    # print(num_warehouses, "# num_warehouses")
 
-num_warehouses = int(lines[3])
-print(num_warehouses, "# num_warehouses")
+    warehouse_stocks = ['stock of items' for i in range(num_warehouses)]
+    warehouse_locations = ['location' for i in range(num_warehouses)]
 
-warehouse_stocks = [ 'stock of items' for i in range(num_warehouses)]
-warehouse_locations = [ 'location' for i in range(num_warehouses)]
+    warehouse_lines = lines[4:4 + (num_warehouses * 2)]
 
-warehouse_lines = lines[4:4 + (num_warehouses * 2)]
+    j = 0
+    for i in range(num_warehouses):
+        warehouse_locations[i] = [int(x) for x in warehouse_lines[j].strip().split()]
+        warehouse_stocks[i] = [int(x) for x in warehouse_lines[j + 1].strip().split()]
+        j += 2
 
-j = 0
-for i in range(num_warehouses):
-    warehouse_locations[i] = [int(x) for x in warehouse_lines[j].strip().split()]
-    warehouse_stocks[i] = [int(x) for x in warehouse_lines[j + 1].strip().split()]
-    j += 2
+        # print(warehouse_locations[i], "# warehouse_location ", i )
+        # print(warehouse_stocks[i], "# warehouse_stock ", i )
 
-    print(warehouse_locations[i], "# warehouse_location ", i )
-    print(warehouse_stocks[i], "# warehouse_stock ", i )
+    # orders
 
-# print(warehouse_stocks, "# warehouse_stocks")
-# print(warehouse_locations, "# warehouse_locations")
+    num_orders_line = 4 + (num_warehouses * 2)
+    num_orders = int(lines[num_orders_line].strip())
+    # print(num_orders, "# num_orders")
 
-# orders
+    order_lines = lines[num_orders_line + 1: num_orders_line + 1 + (num_orders * 3)]
+    orders = [['destination', 'volume', 'product types'] for i in range(num_orders)]
 
-num_orders_line = 4 + (num_warehouses * 2)
-num_orders = int(lines[num_orders_line].strip())
-print(num_orders, "# num_orders")
+    k = 0
+    for i in range(num_orders):
+        for j in range(3):
+            orders[i][j] = [int(x) for x in order_lines[k + j].strip().split()]
+        k += 3
 
-order_lines = lines[num_orders_line + 1: num_orders_line + 1 + (num_orders * 3)]
-orders = [ ['destination', 'volume', 'product types'] for i in range(num_orders) ]
-
-k = 0
-for i in range(num_orders):
-    for j in range(3):
-        orders[i][j] = [int(x) for x in order_lines[k + j].strip().split()]
-    k += 3
-
-    print(orders[i], "# order ", i )
+        # print(orders[i], "# order ", i )
